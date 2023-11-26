618e663b39272bbe52a2e4ee28c1529b9802321a
edmichel2002 <eddymichel2002@gmail.com>
2023-11-26 02:19:10 +0100

    pages ok form ok

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,10 +8,10 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    if(byDateDesc.length>0){
+    if (byDateDesc){
       setTimeout(
         () => setIndex(index < byDateDesc.length-1 ? index + 1 : 0),
         5000
@@ -44,7 +44,7 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${radioIdx+5}`}
+                  key={`${radioIdx + 5}`}
                   type="radio"
                   name="radio-button"
                   checked={idx === radioIdx}
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -2,52 +2,52 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
-
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-          await screen.findByText("Envoyer"),
-          new MouseEvent("click", {
-            cancelable: true,
-            bubbles: true,
-          })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
+    it("a list of fields card is displayed", async () => {
+        render(<Home />);
+        await screen.findByText("Email");
+        await screen.findByText("Nom");
+        await screen.findByText("Prénom");
+        await screen.findByText("Personnel / Entreprise");
     });
-  });
+
+    describe("and a click is triggered on the submit button", () => {
+        it("the success message is displayed", async () => {
+            render(<Home />);
+            fireEvent(
+                await screen.findByText("Envoyer"),
+                new MouseEvent("click", {
+                    cancelable: true,
+                    bubbles: true,
+                })
+            );
+            await screen.findByText("En cours");
+            await screen.findByText("Message envoyé !");
+        });
+    });
 
 });
 
 
 describe("When a page is created", () => {
-  it("a list of events is displayed", () => {
-    render(<Home />);
-    const eventList = screen.getByTestId("event-list");
-    expect(eventList).toBeDefined();
-  })
-  it("a list a people is displayed", () => {
-    render(<Home />);
-    const peopleList = screen.getByTestId("people-list");
-    expect(peopleList).toBeDefined();
-  })
-  it("a footer is displayed", () => {
-    render(<Home />);
-    const testFooter = screen.getByTestId("test-footer");
-    expect(testFooter).toBeDefined();
-
-  })
-  it("an event card, with the last event, is displayed", () => {
-    render(<Home />);
-    const lastEvent = screen.getByTestId("last-event");
-    expect(lastEvent).toBeDefined();
-  })
+    it("a list of events is displayed", () => {
+        render(<Home />);
+        const {container} = render(<Home/>)
+        const listOfEvent = container.querySelector("listOfEvent");
+        expect(listOfEvent).toBeDefined();
+    })
+    it("a list a people is displayed", () => {
+        render(<Home />);
+        const peopleList = screen.getByTestId("people-list");
+        expect(peopleList).toBeDefined();
+    })
+    it("a footer is displayed", () => {
+        render(<Home />);
+        const testFooter = screen.getByTestId("test-footer");
+        expect(testFooter).toBeDefined();
+    })
+    it("an event card, with the last event, is displayed", () => {
+        render(<Home />);
+        const lastEvent = screen.getByTestId("last-event");
+        expect(lastEvent).toBeDefined();
+    })
 });
